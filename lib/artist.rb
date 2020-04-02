ef2112fcdef89fd640db0325c7406c15f9fa11a6
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-04-02 01:48:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,7 +20,6 @@ class Artist
   def self.all
     @@artists
   end
-=begin
   def self.reset_all
     self.all.clear
   end
@@ -28,7 +27,7 @@ class Artist
   def self.count
     self.all.count
   end
-=end
+  
   def add_song(song)
     @songs << song
     song.artist = self
